fb3e18a2dfb728f8d2cb800cd0837b6abe3623a8
Hieu Ho <55455388+qqq12598@users.noreply.github.com>
2021-10-09 09:26:07 +0700

    create app bar widget

---
+++ lib/widgets/app_bar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:lettutor_app/config/theme.dart';
+
+class ApplicationAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+
+  ApplicationAppBar({@required this.title});
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: AppTheme.mainColor,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+        backgroundColor: AppTheme.mainColor2,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          title,
+          style: TextStyle(color: AppTheme.mainColor, fontSize: 28),
+        ));
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(60);
+}
