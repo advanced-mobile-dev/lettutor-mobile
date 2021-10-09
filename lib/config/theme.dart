4f4ce037caddd26c585708139a9c73daa213d97e
Hieu Ho <55455388+qqq12598@users.noreply.github.com>
2021-10-09 09:02:28 +0700

    setup project

---
+++ lib/config/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const backgroundColor = Color(0xffFAFAFA);
+  static const mainColor = Color(0xff08346F);
+  static const mainColor2 = Color(0xffEAF2FD);
+  static const mainColor3 = Color(0xff2F80ED);
+}
