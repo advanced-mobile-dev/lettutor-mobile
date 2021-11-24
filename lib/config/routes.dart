567388f00f200523efa36acd6e106d4ffc7e7e66
hohieuu <55455388+qqq12598@users.noreply.github.com>
2021-11-24 19:16:04 +0700

    add routes config

---
+++ lib/config/routes.dart
@@ -0,0 +1,15 @@
+class LettutorRoutes {
+  static const home = '/';
+  static const loading = 'loading';
+  static const start = 'start';
+  static const signUp = 'sign-up';
+  static const signIn = 'sign-in';
+  static const history = 'history';
+  static const teacherProfile = 'teacher-profile';
+  static const teacherCalendar = 'teacher-calendar';
+  static const booking = 'booking';
+  static const courseDetail = 'course-detail';
+  static const userProfile = 'user-profile';
+  static const changePassword = 'change-password';
+  static const languageSetting = 'language-setting';
+}
